bdadae7f949f222b693bbdfdb3af9064449b41a8
renoud-grappin <loicrg69@gmail.com>
2017-07-30 16:30:23 +0200

    add filters in admin

--- project/admin.py
+++ project/admin.py
@@ -4,11 +4,14 @@ from .models import Tool, TypeOfTool, PushFoward, Newsletter
 # Register your models here.
 
 class ToolAdmin(admin.ModelAdmin):
-	list_display = ('name', 'activate')
-	list_filter = ['activate']
+	list_display = ('name', 'activate', 'pushFowardBoolean')
+	list_filter = ['activate', 'pushFowardBoolean']
+
+class TypeOfToolAdmin(admin.ModelAdmin):
+	list_display = ('name', 'canonical', 'codeColor')
 
 admin.site.register(Tool, ToolAdmin)
-admin.site.register(TypeOfTool)
+admin.site.register(TypeOfTool,TypeOfToolAdmin)
 admin.site.register(PushFoward)
 admin.site.register(Newsletter)
 
